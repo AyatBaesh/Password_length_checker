f63823ca2d51c56489f139fe778ab69d522ef39d
Ayat <ayatbaeshev@gmail.com>
2022-12-08 13:09:13 +0100

    Added message telling if password fits the requirements or not

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,9 @@ password.addEventListener("keyup", () => {
     let result = passwordLengthCheck(password.value)
     if (result) {
         criteria_text.style.color = "green";
+        criteria_text.innerHTML = "Your password fits the requirements";
     } else {
         criteria_text.style.color = "red";
+        criteria_text.innerHTML = "Password should be at least 10 characters and can't contain any spaces";
     }
 })
